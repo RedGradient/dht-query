4bcdbe31fdd407fb47db2c9cfac81c598c054f7a
John T. Wodder II <git@varonathe.org>
2026-01-07 22:51:50 -0500

    Factor out & fix IP regexen

--- src/dht_query/consts.py
+++ src/dht_query/consts.py
@@ -5,3 +5,9 @@ TRANSACTION_ID_LEN = 2
 DEFAULT_TIMEOUT = 15.0
 
 CLIENT = b"TEST"
+
+IPV4_REGEX = r"\d+\.\d+\.\d+\.\d+"
+
+# Allow periods in order to support "mixed" addresses in which the last 32 bits
+# are in IPv4 format, like ":ffff:192.0.2.128"
+IPV6_REGEX = r"[A-Fa-f0-9:.]+"
